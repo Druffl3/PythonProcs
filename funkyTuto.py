09de4ee2fd31dfa1f9638e27949d72623540873d
Goutham R <gouthamr2896@gmail.com>
2020-03-07 14:03:33 +0530

    Sorting function added.

--- funkyTuto.py
+++ funkyTuto.py
@@ -1,4 +1,14 @@
 def theFunco():
-    print("The Func")
+    print("list",sum([0,0,1]))
+    print("tuple",sum((1,2,3)))
+    print("String",sum({1,2,3}))
 
-theFunco()
+l = [5,9,8]
+s = {5,9,8}
+def sortingFunc():
+    print("Sorted",sorted([2,1,3]))
+    print("Sorted",sorted(s))
+    l.sort()
+    print("Sort",l)
+
+sortingFunc()
