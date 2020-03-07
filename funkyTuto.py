16134e3bd3d34d4372d1173690ffa75a8dcb688c
Goutham R <gouthamr2896@gmail.com>
2020-03-07 14:13:55 +0530

    Add mult and noWork functions.

--- funkyTuto.py
+++ funkyTuto.py
@@ -6,9 +6,28 @@ def theFunco():
 l = [5,9,8]
 s = {5,9,8}
 def sortingFunc():
+    #Sorted returns sorted enumerated type
     print("Sorted",sorted([2,1,3]))
     print("Sorted",sorted(s))
     l.sort()
+    #sort modifies the enumerated type
     print("Sort",l)
 
+def addFunc(a):
+    """
+    Adds 1 to a
+    """
+    b = a + 1
+    return b
+
+def mult(a,b):
+    return a*b
+
+def noWork():
+    pass
+
+print(mult(2,2))
+print(mult(2,"aaa"))
+print(mult("a",3))
+noWork()
 sortingFunc()
