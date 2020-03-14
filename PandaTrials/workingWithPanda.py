b2c9b402d793ed3cf9a035fe274500572e98185d
Goutham R <gouthamr2896@gmail.com>
2020-03-14 20:33:51 +0530

    Updated workingWithPanda by maintaing all panda functions in a class.

--- PandaTrials/workingWithPanda.py
+++ PandaTrials/workingWithPanda.py
@@ -2,23 +2,41 @@
 This script introduces to a basic usage of Pandas
 Date: 14March_2020
 Author: Goutham R
+Notes:
+requires pandas and xlrd.
+pip3 install --user pandas
+pip3 install --user xlrd
 """
 import pandas as pd
 
+class pandaHandler(object):
+    def ReadCSVFile(self,fileName: str) -> pd.core.frame.DataFrame:
+        df = pd.read_csv(fileName)
+        return df
+
+    def ReadExcelFile(self,fileName: str) -> pd.core.frame.DataFrame:
+        edf = pd.read_excel(fileName)
+        return edf
+
+    def ReturnTablesFromExcel(self,*titles: list) -> pd.core.frame.DataFrame:
+        """
+        *titles is a tupple object.
+        """
+        fileName = titles[0]
+        titles = list(titles[1:])
+        edf = (pd.read_excel(fileName)).head() #Takes the first five elements only.
+        requiredTables = edf[titles]
+        return requiredTables
+
+global csv_path
+global xlsx_path
+global songs
+
 csv_path = "majorLazor.csv"
 xlsx_path = "majorLazor.xlsx"
-
 songs = {"album":['Thriller','Back in Black', 'The Darkside of the Moon'], "release":['2011','1980','1969'] ,"Length":['00:42:19','00:42:11','00:00:03']}
-
-#df stands for Data Frame
-df = pd.read_csv(csv_path)
-#edf stands for Excel Data Frame
-edf = pd.read_excel(xlsx_path)
-
 dummy_song = ['a','b',('c','d'),3,['e','f']]
 
-#Converting a dictionary to a data frame
-song_frame = pd.DataFrame(songs)
+pH = pandaHandler()
 
-print(song_frame)
-print(type(song_frame))
+print(pH.ReturnTablesFromExcel(xlsx_path,"Title","Album"))
